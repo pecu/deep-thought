ec71aaca0cfa268bfb161c20667904e40540d955
guagua0201 <tobyupu@gmail.com>
2019-06-04 04:06:30 +0800

    add assist search bar in index.php

--- index.php
+++ index.php
@@ -67,6 +67,19 @@
           </form>
         </div>
 
+        <div class="col-md-10 col-lg-8 col-xl-7 mx-auto">
+          <form action="searchresultpage/assist.php" method="POST"> <!--action="" method = "POST"-->
+            <div class="form-row">
+              <div class="col-12 col-md-9 mb-2 mb-md-0">
+		<input type="text" name="name" class="form-control form-control-lg" placeholder="Enter assistant's name...">
+              </div>
+              <div class="col-12 col-md-3">
+                <button type="text" class="btn btn-block btn-lg btn-primary" data-toggle = "collapse" href="#collapsedev">Go</button>
+              </div>
+            </div>
+          </form>
+        </div>
+
       </div>
     </div>
   </header>
